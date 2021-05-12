e57493e7ed45d3dd62bdd70eb17cbb63eba8a39a
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-05-12 14:54:58 +0200

    Update

--- tests/dependencies.test.js
+++ tests/dependencies.test.js
@@ -3,14 +3,14 @@ const { dependencies, devDependencies } = require('../package.json')
 const deps = Object.keys(dependencies)
 const devDeps = Object.keys(devDependencies)
 
-test(`Sjekker at ${deps.length} dependencies er OK`, () => {
+test(`Sjekker at ${deps.length} dependencies lastes riktig`, () => {
   deps.forEach(dep => {
     const module = require(dep)
     expect(module).toBeTruthy()
   })
 })
 
-test(`Sjekker at ${devDeps.length} devDependencies er OK`, () => {
+test(`Sjekker at ${devDeps.length} devDependencies lastes riktig`, () => {
   devDeps.forEach(dep => {
     const module = require(dep)
     expect(module).toBeTruthy()
