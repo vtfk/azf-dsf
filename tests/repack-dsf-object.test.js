37f1419ed80d1922962e4b173b93216e86d657d0
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2021-12-09 14:07:51 +0100

    Added some more tests

--- tests/repack-dsf-object.test.js
+++ tests/repack-dsf-object.test.js
@@ -83,52 +83,63 @@ const personAdditionalAddress = {
   }
 }
 
+const personAdditionalAddress2 = {
+  RESULT: {
+    HOV: {
+      ...person,
+      ADR1: 'Oppholdsboligen på tunet',
+      ADR2: 'POSTBOKS 38738723',
+      ADR3: 'Banan-veien i 2040 Telemark'
+    }
+  }
+}
+
 const justPerson = {
   RESULT: {
-      HOV: {
-        ...person
-      }
+    HOV: {
+      ...person
+    }
   }
 }
 
 const invalidADR3butWithADR = {
   RESULT: {
-      HOV: {
-          ...person,
-          ADR1: "Kjuttaviga",
-          ADR2: "Turid",
-          ADR3: 'KRAKABAKA Krukestad'
-      }
+    HOV: {
+      ...person,
+      ADR1: 'Kjuttaviga',
+      ADR2: 'Turid',
+      ADR3: 'KRAKABAKA Krukestad'
+    }
   }
 }
 
 const invalidADR3POSTN = {
   RESULT: {
-      HOV: {
-          ...personWithoutADR,
-          ADR1: "Kjuttaviga",
-          ADR2: "Turid",
-          ADR3: 'KRAKABAKA Krukestad'
-      }
+    HOV: {
+      ...personWithoutADR,
+      ADR1: 'Kjuttaviga',
+      ADR2: 'Turid',
+      ADR3: 'KRAKABAKA Krukestad'
+    }
   }
 }
 const invalidADR3POSTS = {
   RESULT: {
-      HOV: {
-          ...personWithoutADR,
-          ADR1: "Kjuttaviga",
-          ADR2: "Turid",
-          ADR3: '1999 2020'
-      }
+    HOV: {
+      ...personWithoutADR,
+      ADR1: 'Kjuttaviga',
+      ADR2: 'Turid',
+      ADR3: '1999 2020'
+    }
   }
 }
 const noADR3 = {
   RESULT: {
-      HOV: {
-          ...personWithoutADR,
-          ADR1: "Kjuttaviga",
-          ADR2: "Turid"
-      }
+    HOV: {
+      ...personWithoutADR,
+      ADR1: 'Kjuttaviga',
+      ADR2: 'Turid'
+    }
   }
 }
 
@@ -174,6 +185,19 @@ describe('Return correct address fields when', () => {
     expect(repacked.RESULT.HOV.postAdresse.POSTN).toBe(adr3Split[0])
     expect(repacked.RESULT.HOV.postAdresse.POSTS.toLowerCase()).toBe(adr3Split[1].toLowerCase())
   })
+  test('ADR1-3 is set, postAdresse.POSTN and postAdresse.POSTS is set correctly, even when ADR3 has a weird setup', () => {
+    const repacked = repack(personAdditionalAddress2)
+    expect(repacked.RESULT.HOV.postAdresse.POSTN.length === 4 && !isNaN(repacked.RESULT.HOV.postAdresse.POSTN)).toBe(true)
+    expect(repacked.RESULT.HOV.postAdresse.POSTS.length).toBe(personAdditionalAddress2.RESULT.HOV.ADR3.length-repacked.RESULT.HOV.postAdresse.POSTN.length-1)
+  })
+
+  test('ADR1-3 is not set set (Should have original ADR, POSTS, POSTN in postAdresse)', () => {
+    const repacked = repack(justPerson)
+    expect(typeof repacked.RESULT.HOV.postAdresse).toBe('object')
+    expect(repacked.RESULT.HOV.postAdresse.ADR.toLowerCase()).toBe(justPerson.RESULT.HOV.ADR.toLowerCase())
+    expect(repacked.RESULT.HOV.postAdresse.POSTN).toBe(justPerson.RESULT.HOV.POSTN)
+    expect(repacked.RESULT.HOV.postAdresse.POSTS.toLowerCase()).toBe(justPerson.RESULT.HOV.POSTS.toLowerCase())
+  })
 
   test('ADR, POSTN, POSTS is set', () => {
     const repacked = repack(justPerson)
@@ -189,20 +213,20 @@ test('Returns POSTS and POSTN when no valid zipCode or zipPlace is found in ADR3
   expect(repacked.RESULT.HOV.POSTN.toLowerCase()).toBe(invalidADR3butWithADR.RESULT.HOV.POSTN.toLowerCase())
 })
 
-describe('Returns POSTS "UKJENT" and POSTN "9999" when person do not have POSTS and POSTN and', () => {
+describe('Returns postAdresse object with property-values POSTS: "UKJENT" and POSTN: "9999" when person do not have POSTS and POSTN and', () => {
   test('ADR3 does not contain a valid POSTN', () => {
-      const repacked = repack(invalidADR3POSTN)
-      expect(repacked.RESULT.HOV.POSTS.toLowerCase()).toBe('ukjent')
-      expect(repacked.RESULT.HOV.POSTN).toBe('9999')
+    const repacked = repack(invalidADR3POSTN)
+    expect(repacked.RESULT.HOV.postAdresse.POSTS.toLowerCase()).toBe('ukjent')
+    expect(repacked.RESULT.HOV.postAdresse.POSTN).toBe('9999')
   })
   test('ADR3 does not contain a valid POSTS', () => {
-      const repacked = repack(invalidADR3POSTS)
-      expect(repacked.RESULT.HOV.POSTS.toLowerCase()).toBe('ukjent')
-      expect(repacked.RESULT.HOV.POSTN).toBe('9999')
+    const repacked = repack(invalidADR3POSTS)
+    expect(repacked.RESULT.HOV.postAdresse.POSTS.toLowerCase()).toBe('ukjent')
+    expect(repacked.RESULT.HOV.postAdresse.POSTN).toBe('9999')
   })
   test('ADR3 does not exist', () => {
-      const fn = () => repack(noADR3)
-      expect(fn).toThrow(Error)
+    const fn = () => repack(noADR3)
+    expect(fn).toThrow(Error)
   })
 })
 
